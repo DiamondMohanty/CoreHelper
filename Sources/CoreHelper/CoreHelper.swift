6638c0a03e163e51a4ee565df035988ecd834a6a
DiamondMohanty <memohanty.diamond@gmail.com>
2020-10-27 00:22:36 +0530

    Window Helpers Open

--- Sources/CoreHelper/CoreHelper.swift
+++ Sources/CoreHelper/CoreHelper.swift
@@ -1,3 +1,15 @@
 struct CoreHelper {
     var text = "Hello, World!"
 }
+
+public extension NSView {
+    
+    func pinEdges(to other: NSView) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        leadingAnchor.constraint(equalTo: other.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: other.trailingAnchor).isActive = true
+        topAnchor.constraint(equalTo: other.topAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: other.bottomAnchor).isActive = true
+    }
+    
+}
